3400ad20e9296e70e92d8d39ca4f16bb7d5832d6
Trea Hauet <treahauet@gmail.com>
2013-01-02 03:11:32 -0700

    Gitkeeps and Theme.php

---
+++ theme.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+class Theme_Personal extends Theme
+{
+    public $name            = 'Personal Theme';
+    public $author          = 'Trea Hauet';
+    public $author_website  = 'http://treahauet.com';
+    public $website         = 'http://treahauet.com';
+    public $description     = '';
+    public $version         = '1.0';
+ }
+/* End of file theme.php */
